cee61820894c34666a2ef36f8e7383863bca0d59
ows-ali <45349670+bilalali1@users.noreply.github.com>
2021-06-24 02:39:07 +0500

    add mongo db remote

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
+    .connect(
+        // 'mongodb://127.0.0.1:27017/cinema'
+        'mongodb+srv://ows:ows@cluster0.zo9r2.mongodb.net/cinema?retryWrites=true&w=majority'
+
+
+        , { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
@@ -9,3 +14,4 @@ mongoose
 const db = mongoose.connection
 
 module.exports = db
+
